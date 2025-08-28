ed3b1c35661ba90f67cd7af6458f2cd08df6ec2b
Yinka Okunlola <101950803+okunlolayk@users.noreply.github.com>
2025-08-28 14:29:11 +0100

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,37 +1,37 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'The10XWriter Blog',
+  author: 'Yinka Okunlola',
+  headerTitle: 'The10XWriterBlog',
+  description: 'Research & Write Smarter with AI-Powered Tools',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://the10xwriter.vercel.app',
+  siteRepo: 'https://github.com/okunlolayk/the10xwriter-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
+  //mastodon: 'https://mastodon.social/@mastodonuser',
+  email: 'yinkaokunlola@the10xwriter.com.ng',
+  github: 'https://github.com/okunlolayk',
+  x: 'https://x.com/Okunlolayk',
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
-  medium: 'https://medium.com',
-  bluesky: 'https://bsky.app/',
+  facebook: 'https://facebook.com/okunlolayk',
+  youtube: 'https://www.youtube.com/@yinkaokunlola2126',
+  linkedin: 'https://www.linkedin.com/in/yinka-okunlola/',
+  //threads: 'https://www.threads.net',
+  instagram: 'https://www.instagram.com/yinka.okunlola/',
+  //medium: 'https://medium.com',
+  //bluesky: 'https://bsky.app/',
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
-  stickyNav: false,
+  stickyNav: true,
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    umamiAnalytics: {
+    //umamiAnalytics: {
       // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      //umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
       // You may also need to overwrite the script if you're storing data in the US - ex:
       // src: 'https://us.umami.is/script.js'
       // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
@@ -45,8 +45,8 @@ const siteMetadata = {
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
+    googleAnalytics: {
+    googleAnalyticsId: 'G-8VPF68RLL0', // e.g. G-XXXXXXX
     // },
   },
   newsletter: {
