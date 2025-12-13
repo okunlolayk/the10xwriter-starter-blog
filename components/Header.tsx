fdf80feca26660926b58fc4bdc451d9bf2d3a9d4
Yinka Okunlola <101950803+okunlolayk@users.noreply.github.com>
2025-12-13 07:31:56 +0100

    Update Logo component styling in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,8 +16,8 @@ const Header = () => {
     <header className={headerClass}>
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
-          <div className="mr-3">
-            <Logo />
+          <div className="mr-3 h-8 w-8">
+            <Logo className="h-full w-full" />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
